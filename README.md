4b2b89e4922324021e3624d2f443506488ceb723
Chris Zielinski <ckz8780@gmail.com>
2018-05-08 23:37:58 +0000

    Completed challenge 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Welcome! 
+
+These are the Jasmine testing framework challenges from the Interactive Frontend Development module for CodeInstitute.
+
+The index is at https://ckz8780.github.io/ci-ifd-jasmine-challenges/index.html
---
+++ spec/drinkSpec.js
@@ -0,0 +1,27 @@
+describe("Drink Test", function() {
+    describe("whatCanIDrink function", function() {
+        it('should return "Sorry. I can’t tell what drink because that age is incorrect!" if age is less than 0', function() {
+            expect(whatCanIDrink(-1)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+        
+        it('should return "Drink Toddy" if age is between 0 and 13', function() {
+            expect(whatCanIDrink(0)).toBe("Drink Toddy");
+        });
+        
+        it('should return "Drink Coke" if age is between 14 and 17', function() {
+            expect(whatCanIDrink(15)).toBe("Drink Coke");
+        });
+        
+        it('should return "Drink Toddy" if age is between 18 and 20', function() {
+            expect(whatCanIDrink(19)).toBe("Drink Beer");
+        });
+        
+        it('should return "Drink Toddy" if age is between 21 and 129', function() {
+            expect(whatCanIDrink(22)).toBe("Drink Whiskey");
+        });
+        
+        it('should return "Drink Toddy" if age is 130 or greater', function() {
+            expect(whatCanIDrink(131)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+    });
+});
